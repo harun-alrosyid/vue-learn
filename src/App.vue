ef4542b86ea789f1dec467286d43b67c9b912a6c
harun-alrosyid <alrosyid07@gmail.com>
2025-08-13 14:06:13 +0700

    feat: day 1, learn creat setup dan create component sfc

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TryMe from './components/TryMe.vue';
+
+const name = "harun";
+
+</script>
+
+<template>
+  <header>
+   <h1>vue-learn</h1>
+  </header>
+  <p>{{name}}</p>
+  <TryMe msg="harun"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TryMe.vue
@@ -0,0 +1,23 @@
+<script setup>
+// alternative name
+defineOptions({
+    name: "TryMe"
+})
+// to set up props
+defineProps({ 
+    msg: {
+        type: String,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <div>
+        <p>Try Me {{ msg }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
